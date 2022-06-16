36584cf8aac09dc87797758c4836e93c56f6cf4f
Jamie Adams Fan Account <57022113+JamieAdamsFanAccount@users.noreply.github.com>
2022-06-16 09:25:38 +0100

    First proper commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+
+            {
+                test: /\.(jsx|js|ts|tsx)$/i,
+                exclude: /(node_modules)/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.scss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist')
+        }
+    }
+};
