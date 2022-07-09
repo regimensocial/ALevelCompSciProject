b34d6e379104addc0b0468837b2bf1b8d2176eae
Jamie Adams <jamie.adams.dev4@gmail.com>
2022-07-09 02:18:16 +0100

    made do cool shit, added logic and cleaned shit

--- src/index.ts
+++ src/index.ts
@@ -21,19 +21,44 @@ function initialiseApp(_event: Event): void { // This is the function that will 
         [key: string]: string | number | boolean; // This is a dictionary of styles and their values.
     }
 
+
     class MyElement { // Base element class, it's called MyElement because it would conflict with the inbuilt Element class.
-        name: string; // This is the name of the element, might not serve much purpose besides organisation
+        id: string; // This is the id of the element, MUST BE UNIQUE, used to identify the element and update it.
         type: string; // The actual element this will be
-        innerHTML!: string; // This is what will be inside the element. It's a string but should be HTML.
+        _content!: string | MyElement; // This is what will be inside the element. It's a string but should be HTML.
         className: string; // The class
         events: EventDict; // Any events that will be added to the element.
         properties: PropertiesDict; // Any properties that will be added to the element.
         styling: StyleDict; // Any styling
 
-        generateElement(placeElement?: string): HTMLElement {
+        set content(content: string | MyElement) {
+            this._content = content;
+            
+            this.generateElement(null, true);
+        }
+
+        get content(): string | MyElement {
+            return this._content;
+        }
+
+        updateStyling(styling: StyleDict) { // This will update the styling of the element.
+            this.styling = { ...this.styling, ...styling };
+        }
+
+        generateElement(placeElement?: string, rerender?: boolean): HTMLElement {
             // This will generate the element, check for any events and styling and add them. It also adds the innerHTML. It will also return the element.
+
+            if (rerender && document.querySelectorAll("#" + this.id).length === 0) return; // If the element doesn't exist, don't do anything when trying to rerender.
+
             let element: HTMLElement = document.createElement(this.type);
-            if (this.innerHTML) element.innerHTML = this.innerHTML;
+
+
+            if (this.content && typeof this.content === "string") {
+                element.innerHTML = this.content;
+            } else if (this.content && typeof this.content === "object") {
+                element.appendChild(this.content.generateElement());
+            }
+
             if (this.className) element.classList.add(this.className);
 
             if (this.properties) for (let property in this.properties) {
@@ -48,6 +73,29 @@ function initialiseApp(_event: Event): void { // This is the function that will 
                 element.style[style] = this.styling[style];
             }
 
+            console.log(this.id)
+            console.log(document.getElementById(this.id))
+
+
+            var newID = null;
+            if (rerender && document.querySelectorAll("#" + this.id).length > 1) {
+                
+                // change id to random id
+                newID = "conflict-" + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+
+                console.log("Conflict ID detected, id changed");
+            }
+
+            element.setAttribute("id", newID || this.id);
+
+            if (rerender) {
+                var target: Element = document.querySelectorAll(`#${this.id}`)[0];
+                target.replaceWith(element);
+                console.log(element)
+                if (newID) this.id = newID;
+                return;
+            }
+
             if (placeElement) { // This bit will place the element in the correct place. It takes the argument placeElement, a string, and uses it as a querySelector. If it's not specified, it'll just return the element without placing it.
                 document.querySelector(placeElement).appendChild(element);
             }
@@ -55,46 +103,101 @@ function initialiseApp(_event: Event): void { // This is the function that will 
             return element;
         }
 
-        constructor(name: string, type: string, innerHTML: string, className?: string, events?: EventDict, properties?: PropertiesDict, styling?: StyleDict) {
-            this.name = name;
-            this.type = type;
-            this.innerHTML = innerHTML;
-            this.className = className;
-            this.events = events;
-            this.properties = properties;
-            this.styling = styling;
+        constructor(data: {
+            id: string;
+            type: string;
+            content?: string | MyElement;
+            className?: string;
+            events?: EventDict;
+            properties?: PropertiesDict;
+            styling?: StyleDict;
+        }) { // This is the constructor, it takes the data as an argument, then sets the properties using it.
+            this.id = data.id;
+            this.type = data.type;
+            this._content = data.content;
+            this.className = data.className;
+            this.events = data.events;
+            this.properties = data.properties;
+            this.styling = data.styling;
         }
     }
 
-    class Button extends MyElement { // Button class, used for generic buttons
-        name: string;
+    interface State {
+        [key: string]: any; // should be addressed by a string, can be any value
+    }
+    class MyElementWithState extends MyElement { // This is a class that extends MyElement, it's used to add state to the element.
+        _state: State = {}; // Any data that will be added to the element.
+        get state(): State {
+            return this._state;
+        }
+
+        set state(state: State) {
+            this._state = state;
+            this.generateElement(null, true);
+        }
+
+        constructor(data: {
+            id: string;
+            type: string;
+            content?: string | MyElement;
+            className?: string;
+            events?: EventDict;
+            properties?: PropertiesDict;
+            styling?: StyleDict;
+            initialState?: object;
+        }) { // This is the constructor, it takes the data as an argument, then sets the properties using it.
+            super(data); // unnecessary passing of initialState, but it's easier to keep in.
+            this._state = data.initialState;
+        }
+
+    }
+
+    class Button extends MyElementWithState { // Button class, used for generic buttons
+        id: string;
         text: string;
         func: Function;
-        enabled: boolean = false;
 
         toggle(): void {
-            this.enabled = !this.enabled;
+            this.state.disabled = !this.state.disabled;
+            this.properties.disabled = this._state.disabled;
+            this.generateElement(null, true);
         }
 
-        constructor(name: string, text: string, enabled: boolean, func: Function, className?: string, events?: EventDict, styling?: StyleDict) {
-            events = { ...events, "click": func };
-            super(name, "button", text, className, events, {
-                disabled: !enabled
-            }, styling);
-            this.name = name;
-            this.text = text;
-            this.func = func;
-            this.enabled = enabled;
+        constructor(data: {
+            id: string;
+            text: string;
+            func: Function;
+            className?: string;
+            events?: EventDict;
+            properties?: PropertiesDict;
+            styling?: StyleDict;
+            disabled?: boolean;
+        }) { // This is the constructor, it takes the data as an argument, then sets the properties using it.
+            data.events = data.events = { ...data.events, "click": data.func };
+            super({
+                id: data.id,
+                type: "button",
+                content: data.text,
+                className: data.className,
+                events: data.events,
+                properties: { ...data.properties, disabled: data.disabled },
+                styling: data.styling,
+                initialState: { disabled: data.disabled }
+            });
+            this.id = data.id;
+            this.text = data.text;
+            this.func = data.func;
         }
+
     }
 
     type PositionedWidget = { // positioned widget type. This is used for widgets that are positioned relative to the screen.
         pos: [number, number]; // x and y position. I store them in two-value tuple to make it easier to work with, and because it's better for memory
-        widget: Button | string; // widget to be displayed, either a button or a string.
+        widget: MyElement | string; // widget to be displayed, either a button or a string.
     };
 
     class Menu extends MyElement { // My menu class, which will be used to create a menu. 
-        name: string; // unneeded for now
+        id: string; // unneeded for now
         private title!: string; // The title of the menu
         private description!: string; // the description
         private widgets: (PositionedWidget)[]; // all the widgets to pass, talked about above
@@ -117,114 +220,114 @@ function initialiseApp(_event: Event): void { // This is the function that will 
             menuContainer.appendChild(title);
             menuContainer.appendChild(description);
 
-            let positionedWidgets: PositionedWidget[][] = [];
-
-            this.widgets.forEach((widget: PositionedWidget) => {
-                if (!positionedWidgets[widget.pos[1]]) {
-                    positionedWidgets[widget.pos[1]] = [];
+            // create widgets
+            for (let widget of this.widgets) {
+                // check if widget is a button or a string
+                if (typeof widget.widget === "string") {
+                    let text: HTMLElement = document.createElement('div');
+                    text.innerText = widget.widget;
+                    menu.appendChild(text);
+                } else {
+                    var widgetToGen = widget.widget.generateElement();
+                    // widgetToGen.style.position = "absolute";
+                    // widgetToGen.style.left = widget.pos[0] + "px";
+                    // widgetToGen.style.top = widget.pos[1] + "px";
+                    menu.appendChild(widgetToGen);
                 }
-                positionedWidgets[widget.pos[1]][widget.pos[0]] = widget;
-            });
-
-
-            // find largest X in positionedWidgets
-            console.log(this.widgets)
-
-            const largestX = this.widgets.reduce(function (prev, current) {
-                return (prev.pos[0] > current.pos[0]) ? prev : current
-            }).pos[0];
-
-            // find largest Y in positionedWidgets
-            const largestY = this.widgets.reduce(function (prev, current) {
-                return (prev.pos[1] > current.pos[1]) ? prev : current
-            }).pos[1];
-
-            console.log([largestX, largestY]);
-
-            // loop how many times we need to create a row
-            for (let i = 1; i < positionedWidgets.length; i++) {
-                console.log(i)
-                let row: HTMLElement = document.createElement('div');
-                row.classList.add("row");
-
-                // loop how many times we need to create a column in the row
-
-                for (let j = 1; j < (largestX + 1); j++) {
-
-                    let cell: HTMLElement = document.createElement('div');
-                    cell.classList.add("column", `x${j}`, `y${i}`);
-                    row.appendChild(cell);
-                }
-
-
-                menu.appendChild(row);
-
             }
 
-            positionedWidgets.map((row: PositionedWidget[], i: number) => {
-                row.map((widget: PositionedWidget, j: number) => {
-                    if (widget) {
-                        let position: string = (`.x${j}.y${i}`);
-                        console.log(position)
-
-                        let currentWidget = widget.widget;
-                        if (currentWidget instanceof Button) {
-                            menu.querySelector(position).appendChild(currentWidget.generateElement());
-                        } else {
-                            // create div and append text
-                            let div: HTMLElement = document.createElement('div');
-                            div.innerText = currentWidget;
-                            console.log(position)
-                            console.log(menu)
-                            menu.querySelector(position).appendChild(div);
-                        }
-                    }
-                }
-                )
-            })
 
             menuContainer.appendChild(menu);
-
-            // document.querySelector("#menu").appendChild(menuContainer);
-
             return menu;
 
         }
 
-        constructor(name: string, title: string, description: string, widgets: (PositionedWidget)[], className?: string, events?: EventDict, styling?: StyleDict) {
-            super(name, "div", "", "menu", events, styling);
-            this.name = name;
-            this.title = title;
-            this.description = description;
-            this.widgets = widgets;
-            this.className = className;
-            this.events = events;
-            this.styling = styling;
+        constructor(data: {
+            id: string;
+            title: string;
+            description: string;
+            widgets: (PositionedWidget)[];
+            className?: string;
+            events?: EventDict;
+            properties?: PropertiesDict;
+            styling?: StyleDict;
+        }) { // This is the constructor, it takes the data as an argument, then sets the properties using it.
+            data.events = data.events = { ...data.events, "click": () => { } };
+            super({
+                id: data.id,
+                type: "div",
+                content: "",
+                className: data.className,
+                events: data.events,
+                properties: data.properties,
+                styling: data.styling
+            });
+            this.id = data.id;
+            this.title = data.title;
+            this.description = data.description;
+            this.widgets = data.widgets;
         }
     }
 
-    var menus = {
-        main: new Menu("main", "Main Menu", "This is the main menu", [
+    var exampleWidget: MyElementWithState = new MyElementWithState({
+        id: "exampleWidget",
+        type: "div",
+        content: "Hi there!",
+        className: "exampleWidget",
+        events: { "click": () => { 
+            exampleWidget.state.count = exampleWidget.state.count + 1; 
+            console.log(exampleWidget.state) 
+            exampleWidget.content = (exampleWidget.state.count).toString();
+            console.log(exampleWidget.content)
+        } },
+        properties: {},
+        styling: {},
+        initialState: { count: 0 }
+    });
 
-            {
-                pos: [1, 1],
-                widget: new Button("start", "Start", true, () => {
-                    console.log("start");
-                })
-            },
-            {
-                pos: [2, 1],
-                widget: new Button("start", "Start", true, () => {
-                    console.log("start");
-                })
-            },
-            {
-                pos: [2, 2],
-                widget: new Button("start", "Start", false, () => {
-                    console.log("start");
-                }, "button2")
-            }
-        ]),
+
+    var menus = {
+        main: new Menu({
+            id: "main",
+            title: "Main Menu",
+            description: "This is the main menu",
+            widgets: [
+                {
+                    pos: [0, 0],
+                    widget: new Button({
+                        id: "newGame",
+                        text: "New Game",
+                        func: () => {
+                            console.log("new game");
+                        }
+                    })
+                },
+                {
+                    pos: [0, 50],
+                    widget: exampleWidget
+                },
+                {
+                    pos: [0, 100],
+                    widget: new Button({
+                        id: "options",
+                        text: "Options",
+                        func: () => {
+                            console.log("options");
+                        }
+                    })
+                },
+                {
+                    pos: [0, 150],
+                    widget: new Button({
+                        id: "exit",
+                        text: "Exit",
+                        func: () => {
+                            console.log("exit");
+                        }
+                    })
+                }
+            ]
+        })
     };
 
 
